393cad1dc3f605b0527824393189f99c1f8d2bb0
NGUYEN LONG <long8a1234@gmail.com>
2022-09-01 10:24:18 +0700

    commit

--- excelExtract/views.py
+++ excelExtract/views.py
@@ -13,31 +13,38 @@ def kcToolPage(request):
     files=document.objects.all()
     pdffiles=pdfFile.objects.all().order_by("-id")
     demoPdfFiles=pdfFile.objects.last()
-    unsignedocs=len(pdfFile.objects.filter(signed=False))
+    numberUnsignepdfs=len(pdfFile.objects.filter(signed=False))
     if request.method=='POST':
-        form=uploadDocumentForm(request.POST,request.FILES)
-        if form.is_valid():
-            file=request.FILES.get('document')
-            excelExtract.importDataExcel(file)
-    return render(request,"KCtool/KCTool.html",{"form":form,"files":files,"pdffiles":pdffiles,"demoPdfFiles":demoPdfFiles,"unsignedocs":unsignedocs})
+        print("12312312")
+        try:
+            file=request.FILES['document']
+            print(file)
+            if file:
+                excelExtract.importDataExcel(file)
+        except:
+            pass
+    return render(request,"KCtool/KCTool.html",{"form":form,"files":files,"pdffiles":pdffiles,"demoPdfFiles":demoPdfFiles,"numberUnsignepdfs":numberUnsignepdfs})
 
 
 def waitSignDoc(request):
-    return render(request,"KCtool/waitingsigndoc.html")
+    numberUnsignepdfs=len(pdfFile.objects.filter(signed=False))
+    unsignedpdfs=pdfFile.objects.filter(signed=False)
+    return render(request,"KCtool/waitingsigndoc.html",{"numberUnsignepdfs":numberUnsignepdfs,"unsignedpdfs":unsignedpdfs})
 
 def signedDoc(request):
-    return render(request,"KCtool/signedDoc.html")
+    numberUnsignepdfs=len(pdfFile.objects.filter(signed=False))
+    return render(request,"KCtool/signedDoc.html",{"numberUnsignepdfs":numberUnsignepdfs})
 # def excelToListPdfs(request):  
 #             return Response(request,"KCtool/KCTool.html")
 @api_view(["POST"])
 def getIdList(request):
     if request.method=='POST':
         print(request.data)
-        loaict=request.data['loaict']
-        listId=request.POST.getlist('file')
-        for  f in listId:
-            loaiAccount=request.data['fileID{}'.format(f)]
-            excelExtract.exportFiles(loaict=loaict,fileID=f,loaiAccount=loaiAccount)  
+        # loaict=request.data['loaict']
+        # listId=request.POST.getlist('file')
+        # for  f in listId:
+        #     loaiAccount=request.data['fileID{}'.format(f)]
+        #     excelExtract.exportFiles(loaict=loaict,fileID=f,loaiAccount=loaiAccount)  
         return redirect("KCTool:kcToolPage")
     
     
