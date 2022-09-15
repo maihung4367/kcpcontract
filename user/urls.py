ab38f6719c54a1666ac12c8974d003e352230ed1
NGUYEN LONG <long8a1234@gmail.com>
2022-09-15 14:12:49 +0700

    commit

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
-app_name='KCTool'
+app_name='user'
 urlpatterns = [
   path('', views.loginView, name='login'),
-  path('^logout/$',auth_views.LogoutView.as_view(),name='logout'),
+  path('logout/',auth_views.LogoutView.as_view(),name='logout'),
 ]
