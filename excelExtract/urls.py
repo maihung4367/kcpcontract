4f0f1d68f26781d90d2fc0159cf7126e6839d811
NGUYEN LONG <long8a1234@gmail.com>
2022-09-19 12:30:17 +0700

    commit

--- excelExtract/urls.py
+++ excelExtract/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("sign_pdf", views.sign_pdf, name="sign_pdf"),
     path("send_pdf",views.send_pdf, name="send_pdf"),
     path("delete_file",views.deleteFile, name="delete_file"),
+    path("delete_excel_file",views.deleteExcelFile, name="delete_file_excel"),
     path('update-profile', views.update_profile, name="update_profile")
 
 
