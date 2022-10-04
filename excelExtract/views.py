633a9810125c3fb0e68dfc4513fcc40a33a9ec9d
NGUYEN LONG <long8a1234@gmail.com>
2022-10-04 16:50:20 +0700

    commit

--- excelExtract/views.py
+++ excelExtract/views.py
@@ -281,7 +281,7 @@ def confirm_pdf(request):
 			print(list_id_pdf_file)
 			print(list_id)
 			numberunsignepdfs=len(pdfFile.objects.filter(signed=False,sended=False,confirmed=True))
-			send_email.send_noti_to_partner_sign_by_email2([], "thu.phamnguyen@kcc.com")
+			send_email.send_noti_to_partner_sign_by_email2([], "khang.huynhquoc@kcc.com")
 			return Response({"code":"00"}, status=status.HTTP_200_OK)
 	except:
 		err_mess = sys.exc_info()[0].__name__ + ": "+ str(sys.exc_info()[1])
