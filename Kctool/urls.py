fe59a50bad554b94b6612e1c705dc243ef37c0dd
NGUYEN LONG <long8a1234@gmail.com>
2022-10-13 03:20:39 +0700

    delete index

--- Kctool/urls.py
+++ Kctool/urls.py
@@ -22,7 +22,6 @@ from excelExtract import urls as KCtool_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('project',views.index),
     path('KCTool',include(KCtool_url,namespace="KCTool")),
     path('', include('user.urls',namespace="user")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
